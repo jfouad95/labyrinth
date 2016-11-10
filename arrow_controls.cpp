c99dff3f61a7f45cd4435359ff25d81a7f495205
jfouad95 <jn.fd95@gmail.com>
2016-11-10 18:49:15 -0500

    quit<enter>
    dsfsd

--- arrow_controls.cpp
+++ arrow_controls.cpp
@@ -2,63 +2,73 @@
 
 void initialize() {
 	initscr();
-	(void) raw();
+	raw();
 	noecho();
 	nonl();
 	intrflush( stdscr, FALSE );
-	(void) keypad( stdscr, TRUE );
+	keypad( stdscr, TRUE );
 }
 
 void finalize() { 
 	endwin();
 }
+void delscreen(SCREEN *screen);
+
+int x = 10, y= 20;
+bool a = true;
 
 int main() {
 	int ch;
-	int x = 20, y= 20;
 	initialize();
-	while (1) {
+
+	while (a) {
 		
 		curs_set(0);
 		wclear( stdscr );
 		wmove( stdscr, x, y );
-		waddch( stdscr, ACS_BULLET );
-
-//		wmove( stdscr, 6, 10);
-//		wprintw( stdscr, (char*)"There are %d lines and %d columns.", LINES, COLS );
-	
-//		wmove( stdscr, 10, 0);
-//		waddch( stdscr, '?' );
+		waddch( stdscr, ACS_DIAMOND );
 
 		wrefresh( stdscr );
 
 		ch = wgetch( stdscr);
 		switch (ch) {
 			case KEY_UP: 
-				wrefresh( stdscr );
-				mvaddch( y+1, x, ACS_BULLET );
-				y += 1;
-			case KEY_DOWN:
-				wrefresh( stdscr );
-				mvaddch( y-1, x, ACS_BULLET );
-				y -= 1;
-			case KEY_LEFT:
-				wrefresh( stdscr );
-				mvaddch( y, x-1, ACS_BULLET );
-				x -= 1;			
-			case KEY_RIGHT:
-				wrefresh( stdscr );
-				mvaddch( y, x+1, ACS_BULLET );
-				x += 1;
-			case KEY_ENTER:
-				finalize();
-				return(0);
+//				wrefresh( stdscr );
+//				mvaddch( y, x-1, ACS_DIAMOND );
+				x -= 1;
 				break;
+			case KEY_DOWN:
+//				wrefresh( stdscr );
+//				mvaddch( y, x+1, ACS_DIAMOND );
+				x += 1;
+				break;
+			case KEY_LEFT:
+//				wrefresh( stdscr );
+//				mvaddch( y-1, x, ACS_DIAMOND );
+				y -= 1;
+				break;			
+			case KEY_RIGHT:
+//				wrefresh( stdscr );
+//				mvaddch( y+1, x, ACS_DIAMOND );
+				y += 1;
+				break;
+			case KEY_BACKSPACE:
+				a = false;
+				wrefresh( stdscr );
+				finalize();
+				break;
+			default:
+				break;
+				
+		}
+		if(!a) {
+			endwin();
+			break;
 		}
 	}
 	
-//	finalize();
-//	return 0;
+	finalize();
+	return 0;
 }
 
 
